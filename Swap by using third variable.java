da9872258faceaf00e80a57806418436418c7016
Rutuja Dinkar Chavan <91588325+RutujaDinkar@users.noreply.github.com>
2021-10-12 19:49:49 -0700

    Add files via upload

---
+++ Swap by using third variable.java
@@ -0,0 +1,15 @@
+package Java.com;
+
+public class Swap {
+
+  public static void main(String[] args) {
+  //W. A. p. to swaping two nos by using third variable
+int a=10;
+int b=20;
+int temp=a;
+a=b;
+b=temp;
+System.out.println(a+ " "+b);
+   
+  }
+}
